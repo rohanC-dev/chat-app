9992af0dad1911b34e6480cc9c0e7ae345d15665
rohanC-dev <rohan.chopra241@gmail.com>
2021-02-16 22:14:01 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+
+require("firebase/firestore");
+
+let firebaseConfig = {
+    apiKey: "AIzaSyDFIiRloNB60UENyh8hR7gvrqMS5DgZd8M",
+    authDomain: "rohan-ad71d.firebaseapp.com",
+    projectId: "rohan-ad71d",
+    storageBucket: "rohan-ad71d.appspot.com",
+    messagingSenderId: "469895915995",
+    appId: "1:469895915995:web:a9b4483c00ced373151ffe"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  var db = firebase.firestore();
+
+  db.settings({
+      timestampsInSnapshots: true
+  });
+
+  window.db = db;
 
 createApp(App).use(store).use(router).mount('#app')
--- src/views/FriendlyChat.vue
+++ src/views/FriendlyChat.vue
@@ -92,44 +92,18 @@
         </div>
         <div class="mesgs">
           <div class="msg_history">
-            <div class="incoming_msg">
+            <div v-for="message in messages" class="incoming_msg">
               <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
               <div class="received_msg">
                 <div class="received_withd_msg">
-                  <p>Test which is a new approach to have all
-                    solutions</p>
+                  <p>{{message.message}}</p>
                   <span class="time_date"> 11:01 AM    |    June 9</span></div>
               </div>
             </div>
-            <div class="outgoing_msg">
-              <div class="sent_msg">
-                <p>Test which is a new approach to have all
-                  solutions</p>
-                <span class="time_date"> 11:01 AM    |    June 9</span> </div>
-            </div>
-            <div class="incoming_msg">
-              <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-              <div class="received_msg">
-                <div class="received_withd_msg">
-                  <p>Test, which is a new approach to have</p>
-                  <span class="time_date"> 11:01 AM    |    Yesterday</span></div>
-              </div>
-            </div>
-            <div class="outgoing_msg">
-              <div class="sent_msg">
-                <p>Apollo University, Delhi, India Test</p>
-                <span class="time_date"> 11:01 AM    |    Today</span> </div>
-            </div>
-            <div class="incoming_msg">
-              <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-              <div class="received_msg">
-                <div class="received_withd_msg">
-                  <p>We work directly with our designers and suppliers,
-                    and sell direct to you, which means quality, exclusive
-                    products, at a price anyone can afford.</p>
-                  <span class="time_date"> 11:01 AM    |    Today</span></div>
-              </div>
-            </div>
+            
+            
+            
+            
           </div>
           <div class="type_msg">
             <div class="input_msg_write">
@@ -151,8 +125,32 @@
 
 
 export default {
-  components: {
-
+name: 'FriendlyChat',
+  data(){
+      return{
+          message: null
+      }
+  },
+  methods:{
+      saveMessage(){
+          db.collection("messages").add({
+              message: this.message
+          })
+          this.message = null;
+      },
+      fetchMessages(){
+        db.collection("users").get().then((querySnapshot) => {
+            let allMessages=[];
+            querySnapshot.forEach((doc) => {
+                allMessages.push(doc.data);
+            })
+            this.messages=allMessages;
+        })
+    }
+  },
+    
+  created(){
+      this.fetchMessages();
   }
 }
 </script>
